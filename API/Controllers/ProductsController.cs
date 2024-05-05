1b0b311fd3898756f81f4a5851abe1c6bb02896b
Matt B <52470960+MashuMaru@users.noreply.github.com>
2024-05-05 11:13:22 +0100

    Update to gitignore

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,11 +1,13 @@
 using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
 [ApiController]
+[AllowAnonymous]
 [Route("api/[controller]")]
 public class ProductsController : ControllerBase
 {
@@ -27,5 +29,4 @@ public class ProductsController : ControllerBase
     {
         return await _context.Products.FirstOrDefaultAsync(x => x.Id == id);
     }
-    
 }
