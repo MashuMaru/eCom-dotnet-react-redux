3699c0e00df71abcb255cbb6a577a08de4f10e47
Matt B <52470960+MashuMaru@users.noreply.github.com>
2024-05-17 07:08:04 +0100

    Implements product detail page and routing

--- UI/src/features/catalog/ProductDetails.tsx
+++ UI/src/features/catalog/ProductDetails.tsx
@@ -1,4 +1,4 @@
-import {Typography} from "@mui/material";
+import {Divider, Grid, Table, TableBody, TableCell, TableContainer, TableRow, Typography} from "@mui/material";
 import {useParams} from "react-router-dom";
 import {useEffect, useState} from "react";
 import agent from "../../app/api/agent.ts";
@@ -20,9 +20,42 @@ const ProductDetails = () => {
   if (!product) return <h1>Product not found...</h1>
   
   return (
-    <Typography variant="h2">
-      {product.name}
-    </Typography>
+    <Grid container spacing={2}>
+      <Grid item xs={6}>
+        <img src={product.pictureUrl} alt={product.name} style={{ width: '100%' }}/>
+      </Grid>
+      <Grid item xs={6}>
+        <Typography variant='h3'>{product.name}</Typography>
+        <Divider sx={{ mb: 2 }} />
+        <Typography variant='h4' color='secondary'>£{(product.price / 100).toFixed(2)}</Typography>
+        <TableContainer>
+          <Table>
+            <TableBody>
+              <TableRow>
+                <TableCell>Name</TableCell>
+                <TableCell>{product.name}</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>Description</TableCell>
+                <TableCell>{product.description}</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>Type</TableCell>
+                <TableCell>{product.type}</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>Brand</TableCell>
+                <TableCell>{product.brand}</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>Qty in Stock</TableCell>
+                <TableCell>{product.quantityInStock}</TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Grid>
+    </Grid>
   )
 }
 
