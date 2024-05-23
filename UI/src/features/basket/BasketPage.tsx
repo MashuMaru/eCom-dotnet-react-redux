0b029e6d71aaff704e72a3b269e375e52acd0a0f
Matt B <52470960+MashuMaru@users.noreply.github.com>
2024-05-23 07:57:24 +0100

    Adds basket table with data

--- UI/src/features/basket/BasketPage.tsx
+++ UI/src/features/basket/BasketPage.tsx
@@ -2,7 +2,18 @@ import {useEffect, useState} from "react";
 import {IBasket} from "../../app/interfaces/IBasket.tsx";
 import agent from "../../app/api/agent.ts";
 import Loading from "../../app/layout/Loading.tsx";
-import {Typography} from "@mui/material";
+import {
+  IconButton,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Typography
+} from "@mui/material";
+import {Delete} from "@mui/icons-material";
 
 const BasketPage = () => {
   const [loading, setLoading] = useState<boolean>(true);
@@ -20,12 +31,39 @@ const BasketPage = () => {
   if (!basket) return <Typography variant="h3">Your basket is empty</Typography>
       
   return (
-    <>
-      <h1>Basket</h1>
-      {basket.items.map((item, idx) => (
-        <p key={idx}>{item.name} - {item.quantity}</p>
-      ))}
-    </>
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Product</TableCell>
+            <TableCell align="right">Price</TableCell>
+            <TableCell align="right">Quantity</TableCell>
+            <TableCell align="right">Subtotal</TableCell>
+            <TableCell align="right"></TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {basket.items.map((row) => (
+            <TableRow
+              key={row.name}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {row.name}
+              </TableCell>
+              <TableCell align="right">£{(row.price / 100).toFixed(2)}</TableCell>
+              <TableCell align="right">{row.quantity}</TableCell>
+              <TableCell align="right">£{((row.price / 100) * row.quantity).toFixed(2)}</TableCell>
+              <TableCell align="right">
+                <IconButton color="error">
+                  <Delete />
+                </IconButton>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
   )
 }
 
